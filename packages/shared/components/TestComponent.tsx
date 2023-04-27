bfb5b57a166887931f0281ab493306133785829a
timohuennebeck <hello@timohuennebeck.com>
2023-04-27 12:38:09 +0200

    initial commit

---
+++ packages/shared/components/TestComponent.tsx
@@ -0,0 +1,24 @@
+// packages/shared/components/TestComponent.js
+import React from "react";
+import { Text, View } from "react-native";
+
+const TestComponent = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f0f0f0",
+        padding: 20,
+        borderRadius: 10,
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+        Hello from Shared Component!
+      </Text>
+    </View>
+  );
+};
+
+export default TestComponent;
